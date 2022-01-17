b41a01813f61d309f26533437301114547b41355
WinnieCWu <winniechn7@gmail.com>
2022-01-16 21:49:25 -0800

    updated password generator

--- Develop/script.js
+++ Develop/script.js
@@ -3,29 +3,36 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//must define generatePassword()***
 function generatePassword() {
   var passwordLength = 0; 
-  while (passwordLength <= '8' || passwordLength >= '128') {
+  while (passwordLength <= 8 || passwordLength >= 128) {
     passwordLength = window.prompt('Please pick a password length between 8 through 128 characters.'); 
-    if (passwordLength >= '8' || passwordLength <= '128') {
-      window.confirm ('You have selected an appropriate password length. Please continue to answer the next few prompts.')
+    if (passwordLength >= 8 || passwordLength <= 128) {
+      window.confirm ('Are you sure you would like to select this length?')
+      console.log(passwordLength);
+      break;
     }
   }
-}
+  
 //prompt for for what character types to include, whether or not to include lowercase, uppercase, numeric, and/or special characters
 //when i answer each prompt, my input should be validated and at least 1 character type should be selected
 ///validate and track it by saving it as a variable (or console.log)
-window.confirm('Would you like to include a lowercase character?')
-window.confirm('Would you like to include an uppercase character?')
-window.confirm('Would you like to include a numeric character?')
-window.confirm('Would you like to include a special character?')
+  return passwordLength;
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
     //var password calls the function generatePassword, which we will code
-  
+    window.confirm('Would you like to include a lowercase character?')
+    console.log(password.value);
+    window.confirm('Would you like to include an uppercase character?')
+    console.log(password.value);
+    window.confirm('Would you like to include a numeric character?')
+    console.log(password.value);
+    window.confirm('Would you like to include a special character?')
+    console.log(password.value);
 
   //searching the HTML for id elements called 'password', which in our HTML, is the textArea
   var passwordText = document.querySelector("#password");
@@ -35,7 +42,7 @@ function writePassword() {
   passwordText.value = password;
 
   //when pw is generated, it's either displayed in an alert or written on the page
-  window.alert("Your new password is X");
+  window.alert("Your new password is " + password + " !");
 }
 
 // Add event listener to generate button
