397d8944f5ab2fb910e1f34dcb73aea3ec4fa7c3
WinnieCWu <winniechn7@gmail.com>
2022-01-16 22:23:35 -0800

    updated password generator with prompts

--- Develop/script.js
+++ Develop/script.js
@@ -25,14 +25,6 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
     //var password calls the function generatePassword, which we will code
-    window.confirm('Would you like to include a lowercase character?')
-    console.log(password.value);
-    window.confirm('Would you like to include an uppercase character?')
-    console.log(password.value);
-    window.confirm('Would you like to include a numeric character?')
-    console.log(password.value);
-    window.confirm('Would you like to include a special character?')
-    console.log(password.value);
 
   //searching the HTML for id elements called 'password', which in our HTML, is the textArea
   var passwordText = document.querySelector("#password");
@@ -40,6 +32,28 @@ function writePassword() {
   //when all prompts are answered, a pw is generated that matches the selected criteria
   //will set the result of the generatePassword function
   passwordText.value = password;
+
+  //**how to capture responses to Yes, and input values, to combine with PW generator **//
+  window.prompt('Would you like to include a lowercase character?')
+  if (window.confirm === "yes"){
+    window.prompt('Please enter at least one lowercase character');
+    console.log(passwordText.value);
+  }
+  window.prompt('Would you like to include an uppercase character?')
+  if (window.prompt === "yes"){
+    window.prompt('Please enter at least one uppercase character');
+    console.log(password.value);
+  }
+  window.prompt('Would you like to include a numeric character?')
+  if (window.prompt === "yes"){
+    window.prompt('Please enter at least one numeric character');
+    console.log(password);
+  }
+  window.prompt('Would you like to include a special character?')
+  if (window.prompt === "yes"){
+    window.prompt('Please enter at least one special character');
+    console.log(passwordInput.value);
+  }
 
   //when pw is generated, it's either displayed in an alert or written on the page
   window.alert("Your new password is " + password + " !");
